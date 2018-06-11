42a162827a70234429d8ebe134f5ad46ec5a0355
张浩辰 <zhang.haochen@darcytech.com>
2018-06-11 09:52:01 +0800

    内存映射，zip文件系统

---
+++ src/main/java/io/MemoryMapTest.java
@@ -0,0 +1,58 @@
+package io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.CRC32;
+
+public class MemoryMapTest {
+
+    public static long checksumMappedFile(Path fileName) throws IOException {
+        try (FileChannel channel = FileChannel.open(fileName)){
+            CRC32 crc = new CRC32();
+            int length = (int) channel.size();
+            ByteBuffer byteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, length);
+            for(int p = 0; p < length; p++){
+                int c = byteBuffer.get(p);
+                crc.update(c);
+            }
+            return crc.getValue();
+        }
+    }
+
+    public static long checksumInputStream(Path fileName) throws IOException {
+        try (InputStream inputStream = Files.newInputStream(fileName)){
+            CRC32 crc32 = new CRC32();
+            int c;
+            while ((c = inputStream.read()) != -1){
+                crc32.update(c);
+            }
+            return crc32.getValue();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path deskTop = Paths.get("/Users/zhanghaochen/Desktop");
+        Path todo = deskTop.resolve("todo");
+        long start = System.currentTimeMillis();
+        long crcVale = checksumMappedFile(todo);
+        long end = System.currentTimeMillis();
+        System.out.println("by mapped file: ");
+        System.out.println(crcVale);
+        System.out.println(end - start);
+        start = System.currentTimeMillis();
+        crcVale = checksumInputStream(todo);
+        end = System.currentTimeMillis();
+        System.out.println("by inputstream: ");
+        System.out.println(crcVale);
+        System.out.println(end - start);
+
+
+    }
+
+}
--- src/main/java/io/PathTest.java
+++ src/main/java/io/PathTest.java
@@ -1,12 +1,17 @@
 package io;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class PathTest {
 
@@ -43,6 +48,43 @@ public class PathTest {
         System.out.println(Files.size(todo));
         System.out.println(Files.isDirectory(todo));
         System.out.println(Files.getOwner(todo));
+        BasicFileAttributes attributes = Files.readAttributes(todo, BasicFileAttributes.class);
+        System.out.println(attributes.creationTime());
+
+        // 1.6.6 迭代目录中的文件  try可保证流被关闭
+        try(DirectoryStream<Path> desktopPaths = Files.newDirectoryStream(workPath, "*.java")){
+            for (Path desktopPath : desktopPaths) {
+                System.out.println(desktopPath);
+            }
+        }
+        // TODO: 2018/6/10  并不打印目录
+        Files.walkFileTree(workPath.resolve("tmp"), new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if(attrs.isDirectory()){
+                    System.out.println("dictionary:" + file);
+                }else {
+                    System.out.println("file:" + file);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        Path tmpZip = workPath.resolve("tmp.zip");
+        FileSystem fs = FileSystems.newFileSystem(Paths.get(tmpZip.toString()), null);
+        Files.walkFileTree(fs.getPath("/"), new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println(file);
+                return  FileVisitResult.CONTINUE;
+            }
+        });
+
+
 
     }
 
