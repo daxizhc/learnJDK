25aa9338471718f36339ebb4a12dc4acb322880d
张浩辰 <zhang.haochen@darcytech.com>
2018-06-14 01:52:37 +0800

    正则表达式

--- src/main/java/regularexpression/RegexTest.java
+++ src/main/java/regularexpression/RegexTest.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class RegexTest {
 
+    // ((1?[0-9]):([0-5][0-9]))[ap]m  11:59am
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter pattern:");
@@ -22,21 +23,32 @@ public class RegexTest {
             if(matcher.matches()){
                 System.out.println("Match");
                 int g = matcher.groupCount();
+                if(g > 0 ){
+                    for(int j = 0; j < input.length(); j++){
+                        for(int i = 1; i <= g; i++){
+                            if(matcher.start(i) == j && matcher.end(i) == j){
+                                System.out.print("()");
+                            }
+                        }
+                        for(int i = 1; i <= g; i++){
+                            if(matcher.start(i) == j && matcher.end(i) != j){
+                                System.out.print("(");
+                            }
+                        }
+                        System.out.print(input.charAt(j));
+                        for(int i = 1; i<= g; i++){
+                            if(matcher.start(i) != j+1 && matcher.end(i) == j+1){
+                                System.out.print(")");
+                            }
+                        }
 
-
-
-
+                    }
+                    System.out.println();
+                }
+            }else {
+                System.out.println("No Match");
             }
-
-
-
-
         }
-
-
-
     }
-
-
 
 }
