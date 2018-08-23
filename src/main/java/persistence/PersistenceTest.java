a26ac18bef14c61614a3fc9b803cfaa3d90d4ea5
张浩辰 <zhang.haochen@darcytech.com>
2018-08-24 00:39:46 +0800

    对象序列化

--- src/main/java/persistence/PersistenceTest.java
+++ src/main/java/persistence/PersistenceTest.java
@@ -7,9 +7,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.Test;
+
 public class PersistenceTest {
 
-    public static void main(String[] args) throws IOException {
+    // 比对象序列化使用空间更加小，会消除冗余
+    @Test
+    public void testWrite() throws IOException {
         Path path = Paths.get("/Users/zhanghaochen/Documents/ideaproject/learnJDK/src/main/java/persistence");
         FileOutputStream fileOutPutStream = new FileOutputStream(Files.createFile(path.resolve("student.xml")).toFile());
         XMLEncoder out = new XMLEncoder(fileOutPutStream);
